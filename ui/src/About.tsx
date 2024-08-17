2fb96a06aca3478037ddf268a5adb24dec2c6865
Eve Freeman <eve.freema@gmail.com>
2024-08-17 10:42:01 -0400

    more about

--- ui/src/About.tsx
+++ ui/src/About.tsx
@@ -7,6 +7,19 @@ export default function About() {
                 Please send complaints to <strong>/dev/null</strong> and praise/constructive feedback to eve_f on
                 reddit.
             </p>
+            <p>Whipped up the UI yesterday evening so expect significant changes and probable downtime as I continue to
+                work on it.
+                Currently, the results for "availability" and "holds" are stale by ~1 week, but still useful for my use
+                case.</p>
+            <h3>Roadmap:</h3>
+            <ul>
+                <li>refresh availability automatically (probably in 24-48h increments or something)</li>
+                <li>better search, ability to filter by subject, language, format</li>
+                <li>ability to save favorite libraries (which will show at the top of results)</li>
+                <li>ability to compare two library media collections (difference / intersection); what does LA have that
+                    NY doesn't have? or what do they both have?
+                </li>
+            </ul>
         </div>
     )
 }
